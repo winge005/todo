4ab7ca701d12617b10cde9568d0e429a22d9c4e4
svwpsv@gmail.com <Aljoroja>
2024-01-25 22:06:28 +0100

    add initialized code

---
+++ src/main/java/com/svwpsv/todo/Todo.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.svwpsv.todo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Eigenaar
+ */
+public class Todo {
+    private String id;
+    private String name;
+    private String description;
+    private LocalDate date;
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
+    
+    
+}
